973c7973b4efa0d458f033a2e1ca4e933e7ae9f9
kp1998patel <patel1998kevin@gmail.com>
2021-12-03 01:35:48 -0500

    ACTOR and ACTOR TESTING with 100% Coverage for Word Level Statistics done.

---
+++ app/actors/RepoIssuesActor.java
@@ -0,0 +1,125 @@
+package actors;
+
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+import org.eclipse.egit.github.core.Issue;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.IssueService;
+import org.eclipse.egit.github.core.service.RepositoryService;
+import services.RepoIssues;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+public class RepoIssuesActor extends AbstractActor {
+
+    private RepoAnalyzer analyzer;
+
+    public void setAnalyzer(RepoAnalyzer analyzer) {
+        this.analyzer = analyzer;
+    }
+
+    public static Props props() {
+        return Props.create(RepoIssuesActor.class);
+    }
+
+    public static final class GetReport {
+        private String userName, repoName;
+
+        public GetReport(String userName, String repoName) {
+            this.userName = userName;
+            this.repoName = repoName;
+        }
+    }
+
+    public static final class SetAnalyzer {
+        private RepoAnalyzer analyzer;
+
+        public SetAnalyzer(RepoAnalyzer analyzer) {
+            this.analyzer = analyzer;
+        }
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(GetReport.class, this::onGetReport)
+                .match(SetAnalyzer.class, setAnalyzer -> this.setAnalyzer(setAnalyzer.analyzer))
+                .build();
+    }
+
+    private void onGetReport(GetReport getReport) {
+        CompletableFuture<String> result;
+        if (getReport != null &&
+                getReport.userName != null && !getReport.userName.isBlank() &&
+                getReport.repoName != null && !getReport.repoName.isBlank()) {
+            result = RepoAnalyzer.getIssueReportFromRepo(getReport.userName, getReport.repoName)
+                    .exceptionally(throwable -> "Error");
+        } else {
+            result = CompletableFuture.supplyAsync(() -> "Error");
+        }
+        getSender().tell(result, getSender());
+    }
+
+    public static final class RepoAnalyzer {
+        private static GitHubClient gitHubClient = new GitHubClient();
+        private static IssueService issueService = new IssueService(gitHubClient);
+        private static RepositoryService repositoryService = new RepositoryService(gitHubClient);
+
+        public void setGitHubClient(GitHubClient gitHubClient) {
+            RepoAnalyzer.gitHubClient = gitHubClient;
+        }
+
+        public void setIssueService(IssueService issueService) {
+            RepoAnalyzer.issueService = issueService;
+        }
+
+        public void setRepositoryService(RepositoryService repositoryService) {
+            RepoAnalyzer.repositoryService = repositoryService;
+        }
+
+        public static CompletableFuture<String> getIssueReportFromRepo(String userName, String repo) {
+            return CompletableFuture.supplyAsync(() -> fetchRepoIssues(userName, repo))
+                    .thenApplyAsync(RepoIssues::generateReport)
+                    .exceptionally(throwable -> "Error");
+
+        }
+
+        public static List<Issue> fetchRepoIssues(String userName, String repo) {
+            List<Issue> issues = null;
+            try {
+                Repository repository = repositoryService.getRepository(userName, repo);
+                issues = issueService.getIssues(repository, null);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return issues;
+        }
+
+        public static String generateReport(List<Issue> issues) {
+            if (issues == null)
+                return "Error";
+            if (issues.isEmpty())
+                return "No issues present in the repository";
+            Map<String, Integer> map = issues.stream()
+                    .map(issue -> issue.getTitle().split("\\s+"))
+                    .flatMap(Arrays::stream)
+                    .filter(word -> word.length() > 1)
+                    .collect(Collectors.toMap(w -> w.toLowerCase(), w -> 1, Integer::sum));
+            map = map.entrySet()
+                    .stream()
+                    .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
+
+            StringBuilder builder = new StringBuilder();
+            map.entrySet().stream()
+                    .map(e -> "<b>" + e.getValue() + "</b> => " + e.getKey() + "<br>")
+                    .forEach(builder::append);
+            return builder.toString();
+        }
+
+    }
+}
